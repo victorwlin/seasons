691e26f9ef8da762b3aa212eee4dc419ef93d71c
victorwlin <57220247+victorwlin@users.noreply.github.com>
2020-04-10 20:48:36 +0800

    Add files via upload

---
+++ docs/precache-manifest.3dc31e5fae03e1e5794e82321c73f6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb095bf5a387a31d4fbd9b949aec3027",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "f717648518fe201d289f",
+    "url": "/seasons/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "36079b390712337427b8",
+    "url": "/seasons/static/js/2.db57ba89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.db57ba89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f717648518fe201d289f",
+    "url": "/seasons/static/js/main.a94e286c.chunk.js"
+  },
+  {
+    "revision": "c9aa463c2c8102aff538",
+    "url": "/seasons/static/js/runtime-main.d04c89a8.js"
+  }
+]);
